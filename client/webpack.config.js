c1ecb3f4975fe7c8eafe126d8061e5da0a8f9161
MaxPyvovarov <max.pivovar@gmail.com>
2022-11-08 00:26:51 +0200

    client/webpack-warnings->resolved

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,35 +1,41 @@
-const path = require("path");
-const HTMLPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HTMLPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: "ts-loader",
-          options: {
-            transpileOnly: true,
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"]
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".js", ".ts", ".tsx", ".json", ".mjs", ".wasm"],
-  },
-  plugins: [
-    new HTMLPlugin({
-      template: path.join(__dirname, "src/index.html"),
-    }),
-  ],
-  devServer: {
-    host: '0.0.0.0',
-    port: 3000,
-    allowedHosts: "all",
-  },
+	entry: './src/index.tsx',
+	output: {
+		path: __dirname + '/dist',
+		filename: 'app.js',
+	},
+	mode: 'development',
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: {
+					loader: 'ts-loader',
+					options: {
+						transpileOnly: true,
+					},
+				},
+			},
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
+	resolve: {
+		extensions: ['.js', '.ts', '.tsx', '.json', '.mjs', '.wasm'],
+	},
+	plugins: [
+		new HTMLPlugin({
+			template: path.join(__dirname, 'src/index.html'),
+		}),
+	],
+	devServer: {
+		host: '0.0.0.0',
+		port: 3000,
+		allowedHosts: 'all',
+	},
 };
